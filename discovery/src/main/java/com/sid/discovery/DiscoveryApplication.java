827b155dcedd318212e313e6d5f1367ebbc502e0
Sidaoui <mssidaoui@gmail.com>
2023-07-05 23:52:34 +0100

    fixing_spring_version_and_configure_discovery

--- discovery/src/main/java/com/sid/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/sid/discovery/DiscoveryApplication.java
@@ -2,7 +2,10 @@ package com.sid.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryApplication {
 
