89a722613d9b7cb846d79a9b2ed42e5685e0d8f0
Sidaoui <mssidaoui@gmail.com>
2023-07-08 17:57:54 +0100

    config_the_configserver

--- config-server/src/test/java/com/sid/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/sid/configserver/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sid.configServer;
+package com.sid.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
