2634f2ba184d2ab42c29ad6b0fa1f70349821bb6
Sidaoui <mssidaoui@gmail.com>
2023-07-19 17:13:33 +0100

    auth_service_skeleton

---
+++ auth-service/src/test/java/com/sid/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
